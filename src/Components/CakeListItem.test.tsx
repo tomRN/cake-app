7fd729986910a8a7e9b7f0eccb815edb67c5eea0
Tom Nuttall <tom@missionlabs.co.uk>
2021-07-08 20:06:05 +0100

    feat(cake-list): Add delete button

--- src/Components/CakeListItem.test.tsx
+++ src/Components/CakeListItem.test.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event'
 import CakeListItem from './CakeListItem';
 import { Cake } from '../types';
 
@@ -14,38 +15,54 @@ describe("Cake List Item Component", () => {
     }
 
     it('Renders without throwing', () => {
-        render(<CakeListItem cake={chocolateCake} />);
+        render(<CakeListItem cake={chocolateCake} onDelete={(id: string) => { }} />);
     });
 
     it('Renders the name', () => {
-        render(<CakeListItem cake={chocolateCake} />);
+        render(<CakeListItem cake={chocolateCake} onDelete={(id: string) => { }} />);
         const nameElement = screen.getByText(/Chocolate Cake/i);
         expect(nameElement).toBeInTheDocument();
     });
 
     it('Renders the comment', () => {
-        render(<CakeListItem cake={chocolateCake} />);
+        render(<CakeListItem cake={chocolateCake} onDelete={(id: string) => { }} />);
         const nameElement = screen.getByText(/A delicious chocolate based cake/i);
         expect(nameElement).toBeInTheDocument();
     });
 
 
     it('Renders a number of filled stars equal to the yum factor', () => {
-        render(<CakeListItem cake={chocolateCake} />);
+        render(<CakeListItem cake={chocolateCake} onDelete={(id: string) => { }} />);
         const filledStars = screen.getAllByText("★")
         expect(filledStars.length).toBe(4);
     })
 
     it('Renders a number of un-filled stars equal to 5 minus the yum factor', () => {
-        render(<CakeListItem cake={chocolateCake} />);
+        render(<CakeListItem cake={chocolateCake} onDelete={(id: string) => { }} />);
         const filledStars = screen.getAllByText("☆")
         expect(filledStars.length).toBe(1);
     })
 
     it('Renders the image of the cake', () => {
-        render(<CakeListItem cake={chocolateCake} />);
+        render(<CakeListItem cake={chocolateCake} onDelete={(id: string) => { }} />);
         const imageElem = screen.getByAltText("A picture of the cake")
         expect(imageElem).toHaveAttribute("src", chocolateCake.imageURL);
+    })
+
+    it('Renders a delete button', () => {
+        render(<CakeListItem cake={chocolateCake} onDelete={(id: string) => { }} />);
+        const deleteElem = screen.getByRole("button", { name: /delete/i })
+        expect(deleteElem).toBeInTheDocument();
+    })
+
+    it('Calls the delete function with the cakes ID when the delete button is clicked', () => {
+        const mockDelete = jest.fn();
+        render(<CakeListItem cake={chocolateCake} onDelete={mockDelete} />);
+        expect(mockDelete).toHaveBeenCalledTimes(0);
+        const deleteElem = screen.getByRole("button", { name: /delete/i })
+        userEvent.click(deleteElem);
+        expect(mockDelete).toHaveBeenCalledTimes(1);
+        expect(mockDelete).toHaveBeenCalledWith(chocolateCake.ID);
     })
 })
 
--- src/Components/CakeListItem.tsx
+++ src/Components/CakeListItem.tsx
@@ -1,28 +1,34 @@
 import React from 'react';
 import { Cake } from '../types';
-import { Card, Row, Col } from 'react-bootstrap'
+import { Card, Row, Col, Button } from 'react-bootstrap'
 
 interface CakeListProps {
     cake: Cake
+    onDelete: (id: string) => void
 }
 
 const CakeListItem = (props: CakeListProps) => {
     return <>
-        <Card className="p-2">
-            <Row>
-                <Col xs={12} md={6} className="center">
-                    <img width="100%" src={props.cake.imageURL} alt="A picture of the cake" />
-                </Col>
-                <Col xs={12} md={6}>
-                    <h4 className="mt-2">{props.cake.name}</h4>
-                    <p>{props.cake.comment}</p>
-                    <h3>
-                        {[0, 1, 2, 3, 4].map(num =>
-                            <span key={num}>{(num <= props.cake.yumFactor - 1) ? "★" : "☆"}</span>
-                        )}
-                    </h3>
-                </Col>
-            </Row>
+        <Card>
+            <Card.Header className="text-right p-0">
+                <Button name="delete" size="sm" variant="" onClick={(evt: any) => { props.onDelete(props.cake.ID) }}><span className="sr-only">delete</span>❌</Button>
+            </Card.Header>
+            <Card.Body className="p-2">
+                <Row>
+                    <Col xs={12} md={6} className="center">
+                        <img width="100%" src={props.cake.imageURL} alt="A picture of the cake" />
+                    </Col>
+                    <Col xs={12} md={6}>
+                        <h4 className="mt-2">{props.cake.name}</h4>
+                        <p>{props.cake.comment}</p>
+                        <h3>
+                            {[0, 1, 2, 3, 4].map(num =>
+                                <span key={num}>{(num <= props.cake.yumFactor - 1) ? "★" : "☆"}</span>
+                            )}
+                        </h3>
+                    </Col>
+                </Row>
+            </Card.Body>
         </Card>
     </>
 }
